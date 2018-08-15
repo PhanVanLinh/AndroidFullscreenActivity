8130c2dc8d04751f4f1e8060542649c3b8c4cecf
Phan Van Linh <phanvanlinh.94vn@gmail.com>
2018-08-15 08:21:07 +0700

    first commit

---
+++ app/src/main/java/vn/linh/androidfullscreenactivity/MainActivity.kt
@@ -0,0 +1,17 @@
+package vn.linh.androidfullscreenactivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // For hide navigation bar
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
+        setContentView(R.layout.activity_main)
+    }
+}
